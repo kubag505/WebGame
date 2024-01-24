84e92a2b666d2d5bd00397c823ae4c90f1e067b3
Jakub Grześkowiak <kubag506@o2.pl>
2024-01-24 14:09:50 +0100

    Bandit css
    Fixed positioning, game-info

--- bandit/main.js
+++ bandit/main.js
@@ -149,7 +149,6 @@ const canvasUI = {
   }
   
   function getWinnnings(combo = [], bet = 10) {
-    const res = checkCombo(combo);
     let defRes = 0;
     // user faild
     if (res.weight < 1) return defRes;
@@ -167,31 +166,11 @@ const canvasUI = {
     };
     
     if (winnnings[res.weight] && winnnings[res.weight][res.val]) {
-      return winnnings[res.weight][res.val] * bet * .1;
+      return winnnings[res.val] * GAME.bet*0.01;
     }
     
     return defRes;
   }
   /* return weight combo */
-  function checkCombo(combo = []) {
-    /* (default) user faild */
-    const res = { weight: 0 };
-    /* check on 3 in a row */
-    if (combo[0] === combo[1] && combo[1] === combo[2]) {
-      res.val = combo[1];
-      res.weight = 3;
-    }
-    /* check on 2 in a row */
-    else if (combo[0] === combo[1] || combo[1] === combo[2]) {
-      res.val = combo[1];
-      res.weight = 2;
-    }
-    /* find any value equal 7 */
-    else if ( combo.some((val) => val === 7) ) {
-      res.val = 7;
-      res.weight = 1;
-    }
-  
-    return res;
-  }
+
   
